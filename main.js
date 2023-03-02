83687ba68d4fa1f1e95f11946de833fb291360a8
ThunDerBoltzCodingClass <76979208+ThunDerBoltzCodingClass@users.noreply.github.com>
2023-03-02 18:37:29 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -15,35 +15,42 @@ function gotResult(error, results){
         console.error(error);
     }
 
-    else if(results[0].label=="Bark"){
+    else if(results[0].label!=""){
         console.log(results);
         random_number_r= Math.floor(Math.random()*255)+1;
         random_number_g= Math.floor(Math.random()*255)+1;
         random_number_b= Math.floor(Math.random()*255)+1;
-        document.getElementById("dog").innerHTML= results[0].label;
-        document.getElementById("dog").style.color="rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
-        document.getElementById("dog").innerHTML= results[0].label;
-        document.getElementById("dog").style.color="rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
-        document.getElementById("dog").innerHTML= results[0].label;
-        document.getElementById("dog").style.color="rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
-        document.getElementById("dog").innerHTML= results[0].label;
-        document.getElementById("dog").style.color="rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
+        img1=document.getElementById("img_display");
 
-        img1=document.getElementById("");
+        if(results[0].label=="Background Noise"){
+            img1.src="ear.png";
+        }
 
-        if(results[0].label=="Bark"){
+        else if(results[0].label=="Bark"){
+            document.getElementById("dog").innerHTML= results[0].label;
+            document.getElementById("dog").style.color="rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
+    
             img1.src="dog bark.jpg";
         }
 
         else if(results[0].label=="Meow"){
+            document.getElementById("cat").innerHTML= results[0].label;
+            document.getElementById("cat").style.color="rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
+    
             img1.src="cat meowing.jpg";
         }
 
         else if(results[0].label=="Moo"){
+            document.getElementById("cow").innerHTML= results[0].label;
+            document.getElementById("cow").style.color="rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
+    
             img1.src="cow mooing.png";
         }
 
         else if(results[0].label=="Roar"){
+            document.getElementById("lion").innerHTML= results[0].label;
+            document.getElementById("lion").style.color="rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
+    
             img1.src="lion roar.jpg";
         }
         
